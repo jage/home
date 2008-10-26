3f7935eebcf71428c82aebf6e6f55362cc331253
Johan Eckerström <johan@duh.se>
2008-10-26 16:55:40 +0100

    Add a script that changes the Network Location after the detected WLAN SSID

---
+++ bin/update-location.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Figure out the SSID of the current WLAN
+ssid=`ioreg -l -n AirPortDriver | grep APCurrentSSID | sed 's/^.*= "\(.*\)".*$/\1/; s/ /_/g'`
+
+if [ $ssid = "Grunk" ]; then
+    location="Hemma"
+elif [ $ssid = "LiU" ]; then
+    location="LiU"
+else
+    location="Automatic"
+fi
+
+# Change to the correct location
+new_location=`/usr/sbin/scselect ${location} | sed 's/^.*(\(.*\)).*$/\1/'`
+
+echo ${new_location}
+
+if [ ${location} != ${new_location} ]; then
+    exit 1
+else
+	exit 0
+fi
