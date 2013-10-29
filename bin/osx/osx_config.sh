b54082f4607bde780bc73bf253dfaea32f55cec4
Johan Eckerström <johan@duh.se>
2013-10-29 07:51:30 +0100

    Fix trailing whitespaces

--- bin/osx/osx_config.sh
+++ bin/osx/osx_config.sh
@@ -7,25 +7,25 @@ defaults write com.apple.dock showhidden -bool true
 # Do not reopen windows
 defaults write com.apple.loginwindow TALLogoutSavesState -bool false
 # Enable font smoothing on non-Apple LCDs
-defaults write NSGlobalDomain AppleFontSmoothing -int 2 
+defaults write NSGlobalDomain AppleFontSmoothing -int 2
 # Disable menu bar transparency
-defaults write NSGlobalDomain AppleEnableMenuBarTransparency -bool false 
+defaults write NSGlobalDomain AppleEnableMenuBarTransparency -bool false
 # Set a blazingly fast keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 0.02
 # Hide iDisk in finder
 defaults write com.apple.finder RemoveIDiskFromSidebarOnStartup -bool true
 # Disable Safari’s thumbnail cache for History and Top Sites
-defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2 
+defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
 # Add invitations to iCal
 defaults write com.apple.mail AddInvitationsToICalAutomatically -bool true
 # Mark all conversation messages as read when opening a conversation
 defaults write com.apple.mail ConversationViewMarkAllAsRead -bool true
 # Enable AirDrop over Ethernet and on unsupported Macs running Lion
-defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true 
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
 # Disable Dashboard
 defaults write com.apple.dashboard mcx-disabled -boolean true
 # Show the ~/Library folder
-chflags nohidden ~/Library 
+chflags nohidden ~/Library
 # Avoid creating .DS_Store files on network volumes
 #defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 # Enable key repeat
