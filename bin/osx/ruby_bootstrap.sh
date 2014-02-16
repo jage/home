93c980120b3993c0f497edade9886d0ebc7eb36c
Johan Eckerström <johan@duh.se>
2014-02-16 12:40:39 +0100

    Use consistent indentation

--- bin/osx/ruby_bootstrap.sh
+++ bin/osx/ruby_bootstrap.sh
@@ -2,10 +2,10 @@
 
 brew update
 for formula in chruby ruby-install; do
-	brew list $formula > /dev/null
-	if [ $? -ne 0 ]; then
-		brew install $formula
-	fi
+  brew list $formula > /dev/null
+  if [ $? -ne 0 ]; then
+    brew install $formula
+  fi
 done
 
 ruby-install --no-reinstall ruby 1.8 -- --without-tcl --without-tk
