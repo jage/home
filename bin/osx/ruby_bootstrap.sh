0c3cd070b66b89df72579a759ecb57b9b3d48c3d
Johan Eckerström <johan@duh.se>
2017-03-07 09:19:15 +0100

    Simplify ruby bootstrap, drop old Rubies

--- bin/osx/ruby_bootstrap.sh
+++ bin/osx/ruby_bootstrap.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-brew update
 for formula in chruby ruby-install; do
   brew list $formula > /dev/null
   if [ $? -ne 0 ]; then
@@ -8,22 +7,9 @@ for formula in chruby ruby-install; do
   fi
 done
 
-ruby-install --no-reinstall ruby 1.8 -- --without-tcl --without-tk
-if [ $? -ne 0 ]; then
-  chruby-exec ruby-1.8 -- gem install bundler
-fi
-
-ruby-install --no-reinstall ruby 1.9
-if [ $? -ne 0 ]; then
-  chruby-exec ruby-1.9 -- gem install bundler pry
-fi
-
-ruby-install --no-reinstall ruby 2.0 -- --enable-dtrace
-if [ $? -ne 0 ]; then
-  chruby-exec ruby-2.0 -- gem install bundler pry
-fi
-
-ruby-install --no-reinstall ruby 2.1 -- --enable-dtrace
-if [ $? -ne 0 ]; then
-  chruby-exec ruby-2.1 -- gem install bundler pry
-fi
+for version in 2.1 2.2 2.3 2.4; do
+  ruby-install --no-reinstall ruby $version -- --enable-dtrace
+  if [ $? -ne 0 ]; then
+    chruby-exec ruby-$version -- gem install bundler pry
+  fi
+done
