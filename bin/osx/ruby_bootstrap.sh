35f922d87c307b35310aed7eb5130ec10c205389
Johan Eckerström <johan@duh.se>
2019-05-12 10:38:37 +0200

    Install supported Rubies, speed up install

--- bin/osx/ruby_bootstrap.sh
+++ bin/osx/ruby_bootstrap.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
 for formula in chruby ruby-install; do
-  brew list $formula > /dev/null
-  if [ $? -ne 0 ]; then
-    brew install $formula
-  fi
+  echo "brew '${formula}'" | brew bundle --file=-
 done
 
-for version in 2.1 2.2 2.3 2.4; do
-  ruby-install --no-reinstall ruby $version -- --enable-dtrace
+for version in 2.4 2.5 2.6; do
+  ruby-install --no-reinstall --jobs="$(sysctl -n hw.physicalcpu)" ruby "${version}" -- --enable-dtrace
+
   if [ $? -ne 0 ]; then
-    chruby-exec ruby-$version -- gem install bundler pry
+    chruby-exec "ruby-${version}" -- gem install bundler pry
   fi
 done
