685a14a94e2cdd75142d670929ba994c4b9f9ed8
Johan Eckerström <johan@duh.se>
2019-10-02 20:31:02 +0200

    Update definitions before installing Rubies

--- bin/osx/ruby_bootstrap.sh
+++ bin/osx/ruby_bootstrap.sh
@@ -4,6 +4,7 @@ for formula in chruby ruby-install; do
   echo "brew '${formula}'" | brew bundle --file=-
 done
 
+ruby-install --latest # Update known Ruby versions
 for version in 2.4 2.5 2.6; do
   ruby-install --no-reinstall --jobs="$(sysctl -n hw.physicalcpu)" ruby "${version}" -- --enable-dtrace
 
